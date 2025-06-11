6dbb73f8cb200e8e28f0f2c7a9dbd09da108a007
Muhammad-Rizqan <mrizqann024@gmail.com>
2025-06-11 15:57:49 +0800

    accessor mutator

--- src/latihan3/rumus.java
+++ src/latihan3/rumus.java
@@ -8,5 +8,18 @@ public class rumus {
     public static int hitungLuas(int sisi) {
         return sisi * sisi;
     }
-    
+    public static int hitungKeliling(int sisi) {
+        return 4 * sisi;
+    }
+
+    public static void main(String[] args) {
+        int sisi = 5;
+
+        int luas = hitungLuas(sisi);
+        int keliling = hitungKeliling(sisi);
+
+        System.out.println("Sisi: " + sisi);
+        System.out.println("Luas persegi: " + luas);
+        System.out.println("Keliling persegi: " + keliling);
+    }
 }
